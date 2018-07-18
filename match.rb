a14372e9f4b0f463610c172752e7951eaea872b3
Nirmal B <nirmalbalachundhar2021@u.northwestern.edu>
2018-07-18 00:07:16 -0400

    First commit of match class- Nirmal (This literally contains nothing), just trying to keep everyone up to date)

---
+++ match.rb
@@ -0,0 +1,7 @@
+## This class forms the backbone of the matching algorithm. The class should be used under a Matcher Tab of some type in the web page
+# so that it can form good sets of study groups for different students
+
+class Match
+  @@student_group = Array.new
+
+end
